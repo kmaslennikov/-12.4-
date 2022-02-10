0ea356a421fe141611b7a4537520dad195c5e40e
kmaslennikov <82436599+kmaslennikov@users.noreply.github.com>
2022-02-10 10:47:57 +0000

    closing by mistake

--- main.go
+++ main.go
@@ -15,17 +15,20 @@ func main() {
 	bb.WriteString("Good day!")
 	if err := ioutil.WriteFile(fileName, bb.Bytes(), 0666); err != nil {
 		fmt.Println(err)
+    return
 	}
 
 	file, err := os.Open(fileName)
 	if err != nil {
 		fmt.Println(err)
+    return
 	}
 	defer file.Close()
 
 	result, err := ioutil.ReadAll(file)
 	if err != nil {
 		fmt.Println(err)
+    return
 	}
 	fmt.Printf(string(result))
 }
